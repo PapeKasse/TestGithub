a7cf82d8082435d816d3c1c027d650b44fc09425
PapeKasse <pacokasse55@gmail.com>
2021-03-27 03:26:20 +0000

    Mon premier commit

---
+++ acceuil.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.6">
+    <title>Album example · Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/album/">
+
+    <!-- Bootstrap core CSS -->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+     <link rel="stylesheet" href="monstyle3.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="bootstrap/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="album.css" rel="stylesheet">
+  </head>
+  <body>
+     <header>
+  <div class="collapse bg-dark" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        
+        <div class="col-sm-4 offset-md-1 py-4">
+          <h4 class="text-white">MENU</h4>
+          <ul class="list-unstyled">
+            <li><a href="acceuil.php" class="text-white">ACCEUIL</a></li>
+            <li><a href="index2.php" class="text-white">INSCRIPTION</a></li>
+            <li><a href="connexion.php" class="text-white">CONNEXION</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container d-flex justify-content-between">
+     
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+</header>
+
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1>BIENVENUE</h1>
+      <p class="lead text-muted"></p>
+     
+    </div>
+  </section>
+
+
+
+<div class="badge badge-primary text-wrap" style="width: 100%;align:center" >
+
+<h5 style="text-align: center;">DESCRIPTION</h5>
+ <h6  style="text-align: center;">La mission première des vigile consiste à préserver l'ordre public, c'est-à-dire assurer la paix, la tranquillité et la sécurité dans sa circonscription ou les rétablir.
+
+La mission de sécurité publique s'appuie sur la « prévention de proximité », qui consacre le lien entre le gendarme et son territoire.. Elle contribue à protéger et à rassurer la population et à établir les contacts nécessaires à l'indispensable remontée du renseignement. La prévention de proximité permet d'exercer une présence dissuasive, de déceler tout comportement susceptible de porter atteinte à l'ordre public et d'interpeller les auteurs d'infraction.
+Pour consulter la liste de tous nos vigiles, vous allez devoir vous inscrire sur le formulaire d'inscription(cliquez sur le bouton menu), et une fois terminéé vous pouvez vous connecter et consulter en même temps la liste de tous nos vigiles en service.
+</h6>
+
+</div>
+<footer class="text-muted" id="footeur">
+  <div class="container">
+    
+    <p>Contact:</p>
+    <p>+33 225 45 30</p>
+    <p><a href="https:uahb.com/"></a>uahb@gmail.com<a href="/docs/4.4/getting-started/introduction/"></a></p>
+  </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script></body>
+</html>
